eb38d4109b896a4b33ba9d4f072f94cb84921042
Thomas <tneveu91@gmail.com>
2020-11-05 20:20:39 +0100

    search presta

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Prestations;
 use App\Repository\PrestationsRepository;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
@@ -30,6 +31,55 @@ class HomeController extends AbstractController
             'massage' => $massage,
             'beaute' => $beaute,
             'coiffure' => $coiffure,
+        ]);
+    }
+
+    /**
+     * @Route("/massage", name="massage")
+     */
+    public function massage(PrestationsRepository $prestationsRepository): Response
+    {
+        $massage = $prestationsRepository->findByName('massage');
+
+        return $this->render('home/massage.html.twig', [
+            'controller_name' => 'HomeController',
+            'massage' => $massage,
+        ]);
+    }
+
+    /**
+     * @Route("/beaute", name="beaute")
+     */
+    public function beaute(PrestationsRepository $prestationsRepository): Response
+    {
+        $beaute = $prestationsRepository->findByName('beauté');
+
+        return $this->render('home/beaute.html.twig', [
+            'controller_name' => 'HomeController',
+            'beaute' => $beaute,
+        ]);
+    }
+
+    /**
+     * @Route("/coiffure", name="coiffure")
+     */
+    public function coiffure(PrestationsRepository $prestationsRepository): Response
+    {
+        $coiffure = $prestationsRepository->findByName('coiffure');
+
+        return $this->render('home/coiffure.html.twig', [
+            'controller_name' => 'HomeController',
+            'coiffure' => $coiffure,
+        ]);
+    }
+
+    /**
+     * @Route("/search/{id}", name="search_show", methods={"GET"})
+     */
+    public function prestaShow(Prestations $prestation): Response
+    {
+        return $this->render('prestations/show.html.twig', [
+            'prestation' => $prestation,
         ]);
     }
 }
